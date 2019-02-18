0ac0e521b81d61539eebbeed86882ee28e2458e4
Xenation <xenations.111@gmail.com>
2019-02-18 18:39:14 +0100

    Simplified Input System
    Closes #29

--- CleanRender/NoclipController.cpp
+++ CleanRender/NoclipController.cpp
@@ -19,31 +19,37 @@ void NoclipController::onUpdate() {
 	if (transform == nullptr) return;
 	float dt = Time::deltaTime;
 	float currentSpeed = speed;
-	if (Input::lShiftPressed) {
+	if (Input::getKeyPressed(KeyCode::LEFT_SHIFT)) {
 		currentSpeed *= 4;
 	}
-	if (Input::wPressed) {
+	if (Input::getKeyPressed(KeyCode::W)) {
 		transform->translate(transform->forward() * currentSpeed * dt);
 	}
-	if (Input::sPressed) {
+	if (Input::getKeyPressed(KeyCode::S)) {
 		transform->translate(transform->backward() * currentSpeed * dt);
 	}
-	if (Input::dPressed) {
+	if (Input::getKeyPressed(KeyCode::D)) {
 		transform->translate(transform->right() * currentSpeed * dt);
 	}
-	if (Input::aPressed) {
+	if (Input::getKeyPressed(KeyCode::A)) {
 		transform->translate(transform->left() * currentSpeed * dt);
 	}
-	if (Input::qPressed) {
+	if (Input::getKeyPressed(KeyCode::Q)) {
 		transform->translate(transform->down() * currentSpeed * dt);
 	}
-	if (Input::ePressed) {
+	if (Input::getKeyPressed(KeyCode::E)) {
 		transform->translate(transform->up() * currentSpeed * dt);
 	}
-	if (Input::mouseRightPressed) {
+	if (Input::getMouseDown(MouseButton::RIGHT)) {
+		Input::LockMouse();
+	}
+	if (Input::getMousePressed(MouseButton::RIGHT)) {
 		eulerRot.y += Input::mouseDelta.x * lookSensivity * dt;
 		eulerRot.x += Input::mouseDelta.y * lookSensivity * dt;
 		Input::mouseDelta = Vec2f::zero; // TODO remove when input system better
 		entity->transform->setRotation(Quaternion::euler(eulerRot));
+	}
+	if (Input::getMouseUp(MouseButton::RIGHT)) {
+		Input::UnlockMouse();
 	}
 }
